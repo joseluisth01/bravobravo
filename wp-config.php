a36965204399cd9e5483f8c4847c7af7299f5e98
joseluisth01 <124920964+joseluisth01@users.noreply.github.com>
2025-09-30 13:18:48 +0200

    Añadir archivos del proyecto

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,16 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', "local" );
+define( 'DB_NAME', "bravobravo" );
 
 /** Database username */
 define( 'DB_USER', "root" );
 
 /** Database password */
-define( 'DB_PASSWORD', "root" );
+define( 'DB_PASSWORD', "" );
 
 /** Database hostname */
-define( 'DB_HOST', "localhost" );
+define( 'DB_HOST', "localhost:3307" );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
